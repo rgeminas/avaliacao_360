1699e7b6489533db3e3bb1da0c6048b3eb10247a
Renan Gemignani <renangemignani@gmail.com>
2011-10-20 22:04:14 -0200

    removing some files

--- multisource_feedback/admin.py
+++ multisource_feedback/admin.py
@@ -2,18 +2,7 @@ from models import *
 #from gp_utils.common.models import Board, Member
 from django.contrib import admin
 
-
-class MemberInline(admin.StackedInline):
-    model = Member
-    extra = 3
-
-class BoardAdmin(admin.ModelAdmin):
-    inlines = [MemberInline]
-
-class FeedbackSetAdmin(admin.ModelAdmin):
-    pass
-
 admin.site.register(Member)
 admin.site.register(FeedbackSet)
 admin.site.register(FeedbackMember)
-admin.site.register(Board, BoardAdmin)
+admin.site.register(Board)
